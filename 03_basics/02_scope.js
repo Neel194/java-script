6ebf2ae39432e317bb256a1a2995f573328c4c13
Neel194 <140739204+Neel194@users.noreply.github.com>
2023-10-23 13:04:49 +0000

    changes in scope file

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -15,7 +15,7 @@ if (true) {
 
 
 function one(){
-    const username = "hitesh"
+    const username =neel"
 
     function two(){
         const website = "youtube"
@@ -30,8 +30,8 @@ function one(){
 // one()
 
 if (true) {
-    const username = "hitesh"
-    if (username === "hitesh") {
+    const username = "neel"
+    if (username === "neel") {
         const website = " youtube"
         // console.log(username + website);
     }
